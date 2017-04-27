05dc3416d6c203e2629aac580f9f1ab1aaabfb67
zhangke <zhangkehope@163.com>
2017-04-27 14:15:57 +0800

    First Commoit

---
+++ src/com/coolweather/app/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.coolweather.app.activity;
+
+import com.coolweather.app.R;
+import com.coolweather.app.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
